6dac577de38dc4863cdc22898769f88e5c2a4bd9
sciencectn <sciencectn@gmail.com>
2016-05-19 23:13:57 -0700

    Forgot to add simulator module

---
+++ src/simulator.py
@@ -0,0 +1,38 @@
+import point_follower
+from multiprocessing import Process
+import time
+
+class SwarmSimulator(object):
+    """
+    A wrapper to handle multiple AutoPilot instances in parallel
+    """
+
+    @staticmethod
+    def manage_drone(drone):
+        drone.bringup_drone()
+        drone.arm_and_takeoff(20)
+        while True:
+            drone.update_exploration()
+            time.sleep(1)
+
+    def __init__(self, n=1):
+        """
+        :param n: Number of drones to simulate
+        """
+        self.processes = []
+        self.drones = []
+        for i in xrange(n):
+            self.drones.append(point_follower.AutoPilot())
+
+    def start(self):
+        for drone in self.drones:
+            self.processes.append(Process(target=SwarmSimulator.manage_drone, args=(drone, )))
+            self.processes[-1].start()
+        for p in self.processes:
+            p.join()
+
+
+if __name__ == '__main__':
+    s = SwarmSimulator(5)
+    s.start()
+
