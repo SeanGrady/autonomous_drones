979eb49ae46ff0368eaaf48d3e09ba9832015a74
Sean Grady <vedicmonk@gmail.com>
2016-10-03 00:57:35 -0700

    added example of working rt plot

---
+++ drone_scripts/rtplot_test.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.axis([0, 10, 0, 1])
+plt.ion()
+
+for i in range(10):
+    y = np.random.random()
+    plt.scatter(i, y)
+    plt.pause(0.05)
+
+while True:
+    try:
+        plt.pause(.05)
+    except KeyboardInterrupt:
+        break
