4fe59f9a6a9fded77d6e98220529171783bba95d
Sean Grady <vedicmonk@gmail.com>
2016-10-03 14:44:09 -0700

    RT plotting working now, thanks to Chris's old script showing me the way

--- drone_scripts/matlab_plotting.py
+++ drone_scripts/matlab_plotting.py
@@ -15,11 +15,37 @@ class RTPlotter(object):
     def __init__(self):
         self.establish_database_connection()
         self.read_config('../database_files/mission_setup.json')
-        self.data = self.get_data()
-        self.plot()
+        plt.ion()
+        self.plot_realtime()
+
+    def generate_plot(self):
+        points = self.get_data()
+        data = self.clean_data(points)
+        x = [lat for lat, lon, reading in data]
+        y = [lon for lat, lon, reading in data]
+        z = [reading for lat, lon, reading in data]
+        xmin, xmax = min(x), max(x)
+        ymin, ymax = min(y), max(y)
+        # x is flipped because negative lon is west...?
+        xi = np.linspace(xmin, xmax, 100)
+        yi = np.linspace(ymin, ymax, 100)
+        zi = griddata(x, y, z, xi, yi)
+        CS = plt.contour(xi,yi,zi,15,linewidths=0.5,colors='k')
+        CS = plt.contourf(xi,yi,zi,15,cmap=plt.cm.jet)
+        # colorbar is buggy as hell while in a loop, no time to fuss with now
+        #plt.colorbar()
+        plt.scatter(x,y,marker='o',c='b',s=5)
+        plt.xlim(xmin,xmax)
+        plt.ylim(ymin,ymax)
 
     def plot_realtime(self):
-        pass
+        while True:
+            try:
+                self.generate_plot()
+                plt.pause(0.05)
+            except KeyboardInterrupt:
+                break
+
 
     def establish_database_connection(self):
         db_name = 'mission_data'
@@ -60,81 +86,18 @@ class RTPlotter(object):
                 cast(a.mission_time, Integer) == cast(g.mission_time, Integer),
             ).all()
         return data
-
-    def plot(self, block=True, time=0.05):
-        """
-        Plot the currently stored data as a contour plot using matplotlib
-
-        In the plot:
-        x,y are lon, lat
-        z is the sensor value for that location
-
-        :param block:
-        :return:
-        """
-        self.matplotlib_imported = True
-
-        if len(self.data) < 5:
-            return
-        try:
-            #all_points = [[d.lon, d.lat, d.value] for d in self._data_points]
-            # TODO: figure out how to split this next line properly
-            all_points = [[lon, lat, data['CO2']] for time, data, lat, lon, alt in self.data]
-            all_points.sort()
-            delete = []
-            for i in xrange(len(all_points) - 1):
-                if all_points[i][0:2] == all_points[i + 1][0:2]:
-                    delete.append(i)
-            for i in reversed(delete):
-                all_points.pop(i)
-            print "Removed {0} duplicates for plotting".format(len(delete))
-            coords = [np.array(a[0:2]) for a in all_points]
-            z = [a[2] for a in all_points]
-
-            first = z[0]
-            all_same = True
-            for val in z:
-                if val != first:
-                    all_same = False
-                    break
-            if all_same:
-                print "all values are the same, not plotting"
-                return
-
-            lower_left = np.minimum.reduce(coords)
-            upper_right = np.maximum.reduce(coords)
-            print np.linalg.norm(upper_right - lower_left)
-            if np.linalg.norm(upper_right - lower_left) < 0.00001:
-                print "points are not varied enough, not plotting"
-                return  # Points are not varied enough to plot
-
-            # fig, ax = plt.subplot(1,1)
-            plt.clf()
-            x = [c[0] for c in coords]
-            y = [c[1] for c in coords]
-            xi = np.linspace(lower_left[0], upper_right[0], 200)
-            yi = np.linspace(lower_left[1], upper_right[1], 200)
-            zi = griddata(x, y, z, xi, yi)
-            CS_lines = plt.contour(xi, yi, zi, 15, linewidths=0.5, colors='k')
-            CS_colors = plt.contourf(xi, yi, zi, 15, cmap=plt.cm.rainbow,
-                                     vmax=abs(zi).max(), vmin=-abs(zi).max())
-            cbar = plt.colorbar(CS_colors)
-            cbar.ax.set_ylabel("Value")
-            cbar.add_lines(CS_lines)
-            plt.scatter(x, y, marker='o', c='b', s=5, zorder=10)
-            plt.xlim(lower_left[0], upper_right[0])
-            plt.ylim(lower_left[1], upper_right[1])
-            plt.title('Air data samples')
-            plt.xlabel("Longitude")
-            plt.ylabel("Latitude")
-            if block:
-                plt.plot()
-                plt.show()
-            else:
-                plt.pause(time)
-            
-        except ValueError as e:
-            print e.__repr__()  # STFU
+    
+    def clean_data(self, points):
+        data = [[lat, lon, reading['CO2']] for time, reading, lat, lon, alt in points]
+        data.sort()
+        delete = []
+        for i in xrange(len(data) - 1):
+            if data[i][0:2] == data[i + 1][0:2]:
+                delete.append(i)
+        for i in reversed(delete):
+            data.pop(i)
+        #print "Removed {0} duplicates for plotting".format(len(delete))
+        return data
 
 if __name__ == '__main__':
    rtp = RTPlotter() 
