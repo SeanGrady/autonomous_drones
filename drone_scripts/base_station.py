414d8f0e4d7c4cbf659b7f13a8f293bd297b7863
Sean Grady <vedicmonk@gmail.com>
2016-10-22 11:53:45 -0700

    bugfixes for base station script

--- drone_scripts/base_station.py
+++ drone_scripts/base_station.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime, cast
+from sqlalchemy.orm import sessionmaker, aliased
 from sqlalchemy.ext.declarative import declarative_base
 from models import *
 from code import interact
@@ -13,6 +13,8 @@ from collections import deque
 
 class DroneCoordinator(object):
     def __init__(self, primary_drone_ip, secondary_drone_ip=None):
+        self.co2_threshold = 500
+        self.read_config('../database_files/mission_setup.json')
         self.primary_drone_addr = 'http://' + primary_drone_ip + ':5000/'
         if secondary_drone_ip:
             self.secondary_drone_addr = 'http://' + secondary_drone_ip + ':5000/'
@@ -24,22 +26,29 @@ class DroneCoordinator(object):
         self.areas_of_interest = deque([])
         self.establish_database_connection()
 
+    def read_config(self, filename):
+        with open(filename) as fp:
+            config = json.load(fp)
+        self.mission_name = config['mission_name']
+
     def run_test_mission(self, drone_address):
         mission = self.load_mission(
-            '/Users/seangrady/Desktop/autonomous_drones/launch/better_mission.json'
+            '../launch/better_mission.json'
         )
         self.launch_drone(drone_address)
         self.send_mission(mission, drone_address)
 
     def demo_control_loop(self):
-        grid_mission = self.load_mission('better_mission.json')
-        self.launch_drone(self.primary_drone_addr)
-        self.send_mission(grid_mission, self.primary_drone_addr)
+        grid_mission = self.load_mission('../launch/better_mission.json')
+        #self.launch_drone(self.primary_drone_addr)
+        #self.send_mission(grid_mission, self.primary_drone_addr)
         while True:
             data = self.get_data()
             clean_data = self.clean_data(data)
             self.find_areas_of_interest(clean_data)
+            print self.areas_of_interest
             while self.areas_of_interest:
+                #import pdb; pdb.set_trace()
                 self.investigate_next_area()
             time.sleep(1)
 
@@ -59,7 +68,7 @@ class DroneCoordinator(object):
         r = requests.post(url, mission_string)
         return r
 
-    def create_point_mission(self, action, relative_points, name):
+    def create_point_mission(self, action, relative_point, name):
         mission_dict = { 
             'points': {
                 name: {
@@ -112,10 +121,15 @@ class DroneCoordinator(object):
         return data
 
     def clean_data(self, points):
+        #import pdb; pdb.set_trace()
         data = []
         for time, reading, lat, lon, alt, id in points:
-            if 'co2' in reading and bool(lat):
-                dat = [lat, lon, reading['co2']['CO2'], id]
+            # CHANGE for real vs fake
+            #if 'co2' in reading and bool(lat):
+            if 'CO2' in reading and bool(lat):
+                #print 'found reading'
+                #dat = [lat, lon, reading['co2']['CO2'], id]
+                dat = [lat, lon, reading['CO2'], id]
                 data.append(dat)
         # sort by ascending CO2 value then by latitude, so that we can remove
         # points with duplicate coordinates, keeping the point with the highest
@@ -135,15 +149,18 @@ class DroneCoordinator(object):
         for lat, lon, reading, id in clean_data:
             if reading > self.co2_threshold and id > self.max_id:
                 self.areas_of_interest.append((lat, lon, reading, id))
-        self.max_id = max(self.areas_of_interest, key=lambda point: point[3])
+        if self.areas_of_interest:
+            self.max_id = max(self.areas_of_interest, key=lambda point: point[3])
 
     def investigate_next_area(self):
-        lat, lon, reading = self.areas_of_interest.popleft()
+        #import pdb; pdb.set_trace()
+        lat, lon, reading, id = self.areas_of_interest.popleft()
         name = 'auto_generated_investigation_point_' + str(self.points_investigated)
         mission = self.create_point_mission('go', [lat, lon, self.secondary_height], name)
         # TODO:
         # mission = make a circle mission with michael's thing
-        self.send_mission(mission, self.secondary_drone_addr)
+        print mission
+        #self.send_mission(mission, self.secondary_drone_addr)
         self.points_investigated += 1
 
     @contextmanager
@@ -171,9 +188,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     dc = DroneCoordinator(args.primary_ip, args.secondary_ip)
-    #dc.demo_control_loop()
-    dc.launch_drone(dc.primary_drone_addr)
+    dc.demo_control_loop()
+    #dc.launch_drone(dc.primary_drone_addr)
     #dc.launch_drone(dc.secondary_drone_addr)
-    dc.run_test_mission(dc.primary_drone_addr)
+    #dc.run_test_mission(dc.primary_drone_addr)
     #dc.run_test_mission(dc.secondary_drone_addr)
     interact(local=locals())
