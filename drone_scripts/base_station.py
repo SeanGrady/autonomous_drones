eb7e36c88f4085bba51a42b59b57cdb5df3b2adf
Sean Grady <vedicmonk@gmail.com>
2016-10-28 13:17:58 -0700

    working pre-ucsd demo

--- drone_scripts/base_station.py
+++ drone_scripts/base_station.py
@@ -307,7 +307,8 @@ if __name__ == '__main__':
 
     dc = DroneCoordinator(args.primary_ip, args.secondary_ip, args.threshold)
 
-    dc.demo_control_loop()
+    #dc.demo_control_loop()
+    dc.launch_drone(dc.primary_drone_addr)
     interact(local=locals())
 
     '''
