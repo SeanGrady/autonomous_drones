1d9a1a544d3a334c63912c69b3f67a5a71e347dc
Sean Grady <vedicmonk@gmail.com>
2016-11-30 16:25:16 -0800

    added notes for future readme/docs, added comments and docstrings to base_station.py

--- drone_scripts/base_station.py
+++ drone_scripts/base_station.py
@@ -1,3 +1,31 @@
+"""
+A script to coordinate multiple (currently 2) instances of drone_control.py
+
+This script takes two IP addresses representing two drones as well as a
+threshold. It then launches and sends missions to the drones based on the
+data they collect. While it can run standalone missions and should be easy to
+modify for other purposes, currently its primary function is to run the laptop
+side of the fire-finding demo used at the October 2016 Terraswarm research
+review that was run at UC Berkeley.
+
+The script should be run on the command line as:
+
+python base_station.py 'primaryip' 'secondaryip' threshold
+
+Where the two IP addresses are any valid IPv4 address format, such as
+'192.168.0.1', or 'localhost', and 'threshold' is something like 500. When
+running the demo, the primary ip should point to the drone that flies the grid
+mission, the secondary ip should point to the drone that waits for high
+readings before investigating, and the threshold should be the minimum value of
+the CO2 sensor that will trigger the second drone (~500-600 seems to work well
+in most conditions). If not running the demo , any of these arguments can be
+replaced by an empty string ''.
+
+Command line arguments:
+primary_ip      -- the IP of the primary drone
+secondary_ip    -- the IP of the secondary drone
+threshold       -- an integer, or string which works with int()
+"""
 from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime, cast
 from sqlalchemy.orm import sessionmaker, aliased
 import math
@@ -17,8 +45,23 @@ from itertools import chain, izip
 
 
 class DroneCoordinator(object):
+    """
+    A class to coordinate multiple (currently 2) instances of drone_control.py
+
+    This can be imported from other scripts if needed, but is currently set up
+    to be run from the command line, as described at the top of the file. 
+
+    """
+
     def __init__(self, primary_drone_ip, secondary_drone_ip=None, threshold=500):
-        #self.threshold = 470
+        """
+        Initialize an instance of DroneCoordinator
+
+        If you're thinking about using this from another module, this is a good
+        place to look for things you can set on runtime if desired. For example
+        primary_height and secondary_height, the config file that is read, or
+        how the addresses are constructed from the IPs.
+        """
         self.threshold = int(threshold)
         self.read_config('../database_files/mission_setup.json')
         self.primary_drone_addr = 'http://' + primary_drone_ip + ':5000/'
@@ -26,6 +69,8 @@ class DroneCoordinator(object):
             self.secondary_drone_addr = 'http://' + secondary_drone_ip + ':5000/'
         self.primary_height = 3
         self.secondary_height = 5
+
+        # Things after this shouldn't be set when the class is constructed
         self.max_id = 0
         self.points_investigated = 0
         self.establish_database_connection()
@@ -34,6 +79,7 @@ class DroneCoordinator(object):
         self.mission_generator = MissionGenerator()
 
     def generate_corner_banana(self):
+        """Sometimes you need a corner banana"""
         intervals = range(0, 11, 2)
         south_points = [[0, point] for point in intervals]
         west_points = [[point, 0] for point in intervals] 
@@ -66,6 +112,14 @@ class DroneCoordinator(object):
         '''
 
     def relative_coords(self, lat1, lon1, lat2, lon2):
+        """
+        Return the relative vector in meters between two GPS coordinates
+        
+        This returns the (North, East) distance in meters between two GPS
+        coordinates (lat1, lon1), (lat2, lon2). So if (lat2, lon2) is 10 meters
+        north and three meters east of (lat1, lon1), this returns (10, 3).
+        Distances to the south or west are negative.
+        """
         lat_dist = nav_utils.lat_lon_distance(lat1, lon1, lat2, lon1)
         lon_dist = nav_utils.lat_lon_distance(lat1, lon1, lat1, lon2)
         lat_dist = math.copysign(lat_dist, (lat2 - lat1))
@@ -73,26 +127,39 @@ class DroneCoordinator(object):
         return [lat_dist, lon_dist]
 
     def get_latest_loc(self, drone_name):
+        """Return the latest GPS location of drone_name from the database."""
         if drone_name == 'Beta':
             print 'getting beta data'
             data = self.get_data_beta()
         elif drone_name == 'Alpha':
             data = self.get_data()
-        #print data
-        #points = self.clean_data(data)
         points = data
-        #print points
-        latest_point = max(points, key=lambda point: point[-1])
-        print latest_point
+        # This should probably use keys instead of indexes, otherwise changing
+        # get_data/get_data_beta can break this
+        latest_point = max(points, key=lambda point: point[5])
         latest_loc = [latest_point[1], latest_point[2]]
-        print latest_loc
         return latest_loc
 
     def relative_triangle(self, drone_addr, drone_name, point, radius=2):
+        """Send a drone to fly a triangle around a GPS coordinate and return.
+
+        This is the function intended to let the second drone 'investigate' the
+        points found by the first drone in the demo.
+
+        drone_addr -- address (not IP) of the drone you want to send
+        drone_name -- name of the drone you want to send
+        point -- the point to fly the triangle around, in [lat, lon] format
+        radius -- the radius of the triangle, default 2 meters
+        """
         self.launch_drone(drone_addr)
         time.sleep(20)
         latest_loc = self.get_latest_loc(drone_name)
-        relative = self.relative_coords(latest_loc[0], latest_loc[1], point[0], point[1])
+        relative = self.relative_coords(
+            latest_loc[0],
+            latest_loc[1],
+            point[0],
+            point[1]
+        )
         N = relative[0]
         E = relative[1]
         config_dict = {
@@ -106,6 +173,7 @@ class DroneCoordinator(object):
         self.send_mission(mission, drone_addr) 
 
     def circle_test(self, drone_address, relative):
+        """Fly the drone in a circle to test generation of circle missions."""
         mission_generator = MissionGenerator()
         rel_point = relative['relative']
         N = rel_point[0]
@@ -119,11 +187,19 @@ class DroneCoordinator(object):
         self.send_mission(mission, drone_address)
 
     def read_config(self, filename):
+        """Read a config file to find the current mission_name.
+
+        This is important for database access, you need the mission name so
+        that you can correctly read the data from the current flight(s). This
+        should definitely be the same mission name any currently flying drones
+        are using.
+        """
         with open(filename) as fp:
             config = json.load(fp)
         self.mission_name = config['mission_name']
 
     def run_test_mission(self, filename, drone_address):
+        """Launch a drone and send it on a mission from a file."""
         mission = self.load_mission(
                 filename
         )
@@ -131,6 +207,14 @@ class DroneCoordinator(object):
         self.send_mission(mission, drone_address)
 
     def demo_control_loop(self):
+        """Run the October 2016 Terraswarm demo.
+
+        This function sends the primary drone to fly a grid pattern over a
+        rectangular area. It then monitors the data gathered by that drone and
+        if it crosses the threshold specified when initializing the class, it
+        sends the secondary drone to fly a relative triangle mission around the
+        point where the threshold was crossed.
+        """
         grid_mission = self.load_mission('courtyard1.json')
         self.launch_drone(self.primary_drone_addr)
         self.send_mission(grid_mission, self.primary_drone_addr)
@@ -148,22 +232,33 @@ class DroneCoordinator(object):
             time.sleep(1)
 
     def make_url(self, address, path):
+        """Turn an address (not an IP) into a full URL for use with Flask."""
         url = address + path
         return url
 
     def launch_drone(self, drone_address):
+        """Launch the drone at drone_address."""
         url = self.make_url(drone_address, 'launch')
         start_time = json.dumps({'start_time':time.time()})
         r = requests.post(url, start_time)
         return r
 
     def send_mission(self, mission_json, drone_address):
+        """Send a mission (JSON string) to the drone at drone_address."""
         url = self.make_url(drone_address, 'mission')
         mission_string = json.dumps(mission_json)
         r = requests.post(url, mission_string)
         return r
 
     def create_point_mission(self, action, relative_point, name):
+        """Create a mission (JSON string) and return it.
+
+        action -- any valid action that can be used by the mission interface,
+                  currently probably only works with 'go' and maybe 'land'.
+        relative_point -- point in NED format to be used for the mission
+        name -- the name of the point (points in missions need names,
+                this can be whatever string your heart desires)
+        """
         mission_dict = { 
             'points': {
                 name: {
@@ -183,17 +278,25 @@ class DroneCoordinator(object):
         return mission_dict
 
     def load_mission(self, filename):
+        """Load a mission (JSON string) from a JSON file."""
         with open(filename) as fp:
             mission = json.load(fp)
         return mission
 
     def establish_database_connection(self):
+        """Setup the database connection through the sqlalchemy interface."""
         db_name = 'mission_data'
         db_url = 'mysql+mysqldb://root:password@localhost/' + db_name
         self.engine = create_engine(db_url)
         self.Session = sessionmaker(bind=self.engine)
 
     def get_data(self):
+        """Return all air sensor data from drone Alpha for current mission.
+
+        This uses the sqlalchemy interface described in models.py to query the
+        database for all the air sensor data from drone Alpha and the current
+        mission, and match each air sensor reading with a GPS location.
+        """
         with self.scoped_session() as session:
             a = aliased(AirSensorRead)
             g = aliased(GPSSensorRead)
@@ -217,18 +320,16 @@ class DroneCoordinator(object):
         return data
 
     def get_data_beta(self):
+        """Return all GPS readings from drone Beta for the current mission."""
         with self.scoped_session() as session:
-            #a = aliased(AirSensorRead)
             g = aliased(GPSSensorRead)
             data = session.query(
-                #cast(a.time, Integer),
                 cast(g.time, Integer),
-                #a.air_data,
                 g.latitude,
                 g.longitude,
                 g.altitude,
+                g.relative,
                 g.id,
-                #a.id,
             ).join(
                 g.mission,
                 g.drone,
@@ -239,7 +340,13 @@ class DroneCoordinator(object):
         return data
 
     def clean_data(self, points):
-        #import pdb; pdb.set_trace()
+        """Take list of records from the database and return list of points
+
+        This function 'cleans' the data fetched from the database by get_data
+        or get_data_beta (or anything else that fetches database data). It
+        removes duplicate records, erroneous/fake/nonsense records, and formats
+        the data into a nice list.
+        """
         data = []
         for time, reading, lat, lon, alt, id, relative in points:
             # CHANGE for real vs fake
@@ -271,6 +378,16 @@ class DroneCoordinator(object):
         return data
     
     def find_areas_of_interest(self, clean_data):
+        """Add any sensor readings over the threshold to the list of AoIs.
+
+        This compares all the CO2/GPS pairs in clean_data and adds any readings
+        that are above the threshold and haven't been seen yet to the internal
+        list of points to investigate. It keeps track of the highest database
+        id it's seen so that it knows which points have been looked at before
+        and which are new since the last time get_data was called.
+
+        clean_data -- database data returned from self.clean_data()
+        """
         for lat, lon, reading, id in clean_data:
             if (int(reading) > self.threshold) and (id > self.max_id):
                 print "found interesting thing", reading
@@ -280,7 +397,7 @@ class DroneCoordinator(object):
             self.max_id = max(self.areas_of_interest, key=lambda point: point[3])
 
     def investigate_next_area(self):
-        #import pdb; pdb.set_trace()
+        """Send the secondary drone to investigate the next AoI."""
         lat, lon, reading, id = self.areas_of_interest.popleft()
         '''
         name = 'auto_generated_investigation_point_' + str(self.points_investigated)
@@ -298,6 +415,7 @@ class DroneCoordinator(object):
 
     @contextmanager
     def scoped_session(self):
+        """Provide a context manager for database access."""
         session = self.Session()
         try:
             yield session
@@ -309,6 +427,7 @@ class DroneCoordinator(object):
             session.close()
 
     def get_ack(self, drone_address):
+        """Request (and return) an acknowledgement from the drone."""
         url = self.make_url(drone_address, 'ack')
         r = requests.get(url)
         return r
@@ -321,7 +440,6 @@ if __name__ == '__main__':
     #parser.add_argument('filename')
     parser.add_argument('threshold')
     args = parser.parse_args()
-    print "***CHECK MAVERICK FOR FAKE AIR SENSOR, DOUBLE GPS FREQ***"
 
     dc = DroneCoordinator(args.primary_ip, args.secondary_ip, args.threshold)
 
@@ -336,4 +454,3 @@ if __name__ == '__main__':
     '''
 
     #dc.relative_triangle(dc.secondary_drone_addr, 'Beta', [32.99111557, -117.127052307])
-
